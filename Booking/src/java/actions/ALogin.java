2ee9cad9a741a51fc8a4a209447882884978ea44
mrcarry01 <108233373+mrcarry01@users.noreply.github.com>
2024-05-30 12:10:37 +0200

    Avance struts.xml y vistas

---
+++ Booking/src/java/actions/ALogin.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author Garrido
+ */
+public class ALogin extends ActionSupport {
+    
+    public ALogin() {
+    }
+    
+    public String execute() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
