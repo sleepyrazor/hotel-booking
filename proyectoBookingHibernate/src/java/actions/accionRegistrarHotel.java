67f6b4223cfc9dae56c118a8148aa57b2b4cf848
Andrea Garcia <andreagarcas811@gmail.com>
2024-06-02 17:42:02 +0200

    update

--- proyectoBookingHibernate/src/java/actions/accionRegistrarHotel.java
+++ proyectoBookingHibernate/src/java/actions/accionRegistrarHotel.java
@@ -6,18 +6,64 @@
 package actions;
 
 import com.opensymphony.xwork2.ActionSupport;
+import modelo.DAO.HotelDAO;
+import modelo.Hotel;
 
 /**
  *
  * @author agarc
  */
 public class accionRegistrarHotel extends ActionSupport {
-    
+
+    private String nombre;
+    private String direccion;
+    private Integer estrellas;
+    private Hotel hotel; // Asegúrate de tener un objeto Hotel con getters y setters
+
     public accionRegistrarHotel() {
     }
-    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Integer getEstrellas() {
+        return estrellas;
+    }
+
+    public void setEstrellas(Integer estrellas) {
+        this.estrellas = estrellas;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    public String registrarHotel() {
+        // Lógica para validar y guardar el nuevo hotel en la base de datos
+        HotelDAO hotelDao = new HotelDAO();
+        hotelDao.guardarHotel(hotel);
+        return SUCCESS;
+    }
+
     public String execute() throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
 }
--- proyectoBookingHibernate/web/Vistas/registroHotel.jsp
+++ proyectoBookingHibernate/web/Vistas/registroHotel.jsp
@@ -3,7 +3,7 @@
     Created on : 02-jun-2024, 17:36:07
     Author     : agarc
 --%>
-
+<%@ taglib prefix="s" uri="/struts-tags" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -13,5 +13,13 @@
     </head>
     <body>
         <h1>Hello World!</h1>
+        <s:form action="registroHotel">
+            <s:textfield key="hotel.nombre" label="Nombre del hotel" />
+            <s:textfield key="hotel.direccion" label="Dirección" />
+            <s:select name="idRol" label="Rol" list="roles" listKey="idRol" listValue="nombreRol" />
+            <!-- Otros campos del formulario -->
+            <s:submit value="Registrar" />
+        </s:form>
+
     </body>
 </html>
